f7cb76d8648013e64c212692ad136bc9bd611587
DKreick <damian.kreick@gmail.com>
2019-05-14 13:16:46 -0300

    Adds sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.scss';
-import ButtonAppBar from './components/navbar';
+import ButtonAppBar from './layout/Navbar';
+import Sidebar from './layout/Sidebar';
+import GameCard from './components/GameCard';
 
 function App() {
   return (
-    <div className="App">
-      <ButtonAppBar/>
+    <div className="app">
+      <ButtonAppBar />
+      <Sidebar />
+      <div className="game-container">
+        <GameCard />
+        <GameCard />
+        <GameCard />
+      </div>
     </div>
   );
 }
---
+++ src/layout/Sidebar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import AvatarIcon from '@material-ui/icons/AccountCircle';
+import GameIcon from '@material-ui/icons/VideogameAsset';
+
+const drawerWidth = 64;
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: '#27ade3',
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+function Sidebar(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem>
+            <ListItemIcon><AvatarIcon /></ListItemIcon>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon><GameIcon /></ListItemIcon>
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+}
+
+Sidebar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Sidebar);
