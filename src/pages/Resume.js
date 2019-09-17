5a82321d84d1d6829da23f52dfb582d2a8881f67
DKreick <damian.kreick@gmail.com>
2019-09-17 11:58:53 -0300

    Adds snackbar to download resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,13 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CvComponent from '../components/Cv/CvComponent';
+import Snackbar from '@material-ui/core/Snackbar';
+import { SnackbarContent } from '@material-ui/core';
+import FlagEs from './../assets/flag-es.jpg';
+import FlagUs from './../assets/flag-us.jpg';
+import CV from './../assets/CV.pdf';
 
 const styles = theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap',
-    marginTop: '80px',
-    justifyContent: 'center',
+    marginTop: '90px',
+    justifyContent: 'center'
+  },
+  download: {
+    backgroundColor: '#27ade3'
+  },
+  link: {
+    color: 'white',
+    display: 'contents',
+    fontWeight: 'bold',
+    textDecoration: 'none'
+  },
+  flag: {
+    borderRadius: '5px',
+    margin: '0px 10px',
+    verticalAlign: 'middle',
+    width: '32px'
   }
 });
 
@@ -18,7 +38,23 @@ export function Resume(props) {
 
   return (
     <div className={classes.container}>
-      <CvComponent/>
+      <CvComponent />
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        open={true}
+      >
+        <SnackbarContent
+          className={classes.download}
+          message={
+          <div>
+            <p className={classes.link}>Download my CV here: </p> 
+            <a href={CV} download="Damian Kreick CV" className={classes.link}><img src={FlagUs} alt="us" className={classes.flag}/>EN</a>
+            <a href={CV} download="Damian Kreick CV" className={classes.link}><img src={FlagEs} alt="es" className={classes.flag}/>ES</a>
+          </div>}
+        /></Snackbar>
     </div>
   );
 }
