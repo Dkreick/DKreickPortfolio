53cc5843fa606a16dced01d38a8afbcdcea3a264
DKreick <damian.kreick@gmail.com>
2019-09-20 13:00:56 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.5d079145d5270af18807896be58284ff.js"
+  "/Portfolio/precache-manifest.551dce4b7c676199a7d43074ce0be38d.js"
 );
 
 self.addEventListener('message', (event) => {
