5b0a591a7fda73495a415df7901ff191c2e18627
dkreick <damian.kreick@gmail.com>
2019-10-11 15:29:40 -0300

    Tunes up CV

--- src/components/Resume/ValuableSkills.js
+++ src/components/Resume/ValuableSkills.js
@@ -6,16 +6,22 @@ class ValuableSkills extends React.Component {
     return (
       <div className="container">
         <p className="title">Valuable Skills</p>
+
         <p className="charge">Programming Languages</p>
         <p className="subTitle">Javascript, C#, C++, ActionScript3, Python.</p>
+
         <p className="charge">Web Developent Tools</p>
-        <p className="subTitle">HTML5, SASS, Bootstrap, Angular 7, React.</p>
+        <p className="subTitle">HTML5, SASS, Bootstrap, Angular 7, ReactJs.</p>
+        
         <p className="charge">Version Control and IDEs</p>
-        <p className="subTitle">Github, Gitlab, Git, VSCode, Sublime, Tower.</p>
+        <p className="subTitle"> Git, Github, Gitlab, Bitbucket, VSCode, Sublime, Tower.</p>
+        
         <p className="charge">Design</p>
-        <p className="subTitle">Illustrator, Photoshop, Material design.</p>
+        <p className="subTitle">Illustrator, Photoshop, Audacity, Sony Vegas, Material UI.</p>
+
         <p className="charge">Unit Testing</p>
         <p className="subTitle">Karma, Jasmine, Cucumber.</p>
+
         <p className="charge">Methodologies</p>
         <p className="subTitle">Kanban, Agile Scrum, TDD, BDD.</p>
       </div>
