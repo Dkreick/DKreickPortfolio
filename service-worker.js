5047d1caabac07229cf136af150b571e0249edc9
dkreick <damian.kreick@gmail.com>
2019-10-17 16:50:54 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@ importScripts(
 );
 
 importScripts(
-  '/Portfolio/precache-manifest.9681f035690f2e381c5c41a85553d0e8.js'
+  '/Portfolio/precache-manifest.458999f787f0b1bf890894477237f0a8.js'
 );
 
 self.addEventListener('message', event => {
