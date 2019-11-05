2046741dbfc12ca869ba1c3482c3439e235e5f8f
DKreick <damian.kreick@gmail.com>
2019-11-05 17:12:51 -0300

    Improves Resume section

--- src/pages/Resume/CvComponent.js
+++ src/pages/Resume/CvComponent.js
@@ -10,9 +10,7 @@ import Courses from './Courses';
 import ValuableSkills from './ValuableSkills';
 import Languages from './Languages';
 import Hobbies from './Hobbies';
-import Snackbar from '@material-ui/core/Snackbar';
-import { SnackbarContent } from '@material-ui/core';
-import FlagUs from './../../assets/icons/flag-us.jpg';
+import Button from '@material-ui/core/Button';
 import Cv from './../../assets/Cv.pdf';
 
 const styles = {
@@ -20,33 +18,21 @@ const styles = {
     border: 'solid 1px black',
     boxShadow: '-10px 10px 5px 0px rgba(0,0,0,0.75)',
     maxWidth: '1280px',
-    margin: 'auto',
+    margin: 'auto'
   },
   download: {
     backgroundColor: '#27ade3',
+    marginTop: '50px',
+    textTransform: 'none'
   },
   link: {
     color: 'white',
     display: 'contents',
     fontWeight: 'bold',
-    textDecoration: 'none',
-  },
-  flag: {
-    borderRadius: '5px',
-    margin: '0px 10px',
-    verticalAlign: 'middle',
-    width: '32px',
+    textDecoration: 'none'
   },
   grid: {
     padding: '20px 40px',
-  },
-  snackbar: {
-    margin: 'auto',
-    left: '0px',
-    top: '30px',
-    position: 'relative',
-    width: 'fit-content',
-    zIndex: 'auto',
   },
 };
 
@@ -55,47 +41,33 @@ class CvComponent extends React.Component {
     const { classes } = this.props;
 
     return (
-      <Card className={classes.card}>
-        <Header />
-        <HeaderActions />
-        <Grid container spacing={8} className={classes.grid}>
-          <Grid item xs={6}>
-            <WorkExperience />
-            <ValuableSkills />
+      <div>
+        <Card className={classes.card}>
+          <Header />
+          <HeaderActions />
+          <Grid container spacing={8} className={classes.grid}>
+            <Grid item xs={6}>
+              <WorkExperience />
+              <ValuableSkills />
+            </Grid>
+            <Grid item xs={6} className={classes.grid}>
+              <Education />
+              <Courses />
+              <Languages />
+              <Hobbies />
+            </Grid>
           </Grid>
-          <Grid item xs={6} className={classes.grid}>
-            <Education />
-            <Courses />
-            <Languages />
-            <Hobbies />
-            <Snackbar
-              className={classes.snackbar}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              open={true}
-            >
-              <SnackbarContent
-                className={classes.download}
-                message={
-                  <div>
-                    <p className={classes.link}>Download my CV here: </p>
-                    <a
-                      href={Cv}
-                      download="Damian Kreick CV"
-                      className={classes.link}
-                    >
-                      <img src={FlagUs} alt="us" className={classes.flag} />
-                      English version!
-                    </a>
-                  </div>
-                }
-              />
-            </Snackbar>
-          </Grid>
-        </Grid>
-      </Card>
+        </Card>
+        <Button className={classes.download}>
+          <a
+            href={Cv}
+            download="Damian Kreick CV"
+            className={classes.link}
+          >
+            <p className={classes.link}>Click Here to download a PDF Version!</p>
+          </a>
+        </Button>
+      </div>
     );
   }
 }
