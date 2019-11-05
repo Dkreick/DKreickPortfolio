d3ee33bea2eccd05291fd1e17a58a7a414103e09
DKreick <damian.kreick@gmail.com>
2019-11-05 15:04:41 -0300

    Installs husky

---
+++ build/precache-manifest.c4c01c96398c83a4ae5fafac5529bf48.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d500d5da6b04105b8ce1801ba5a0d7c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1201625ec3a3eaf11d",
+    "url": "/static/css/main.69926dd9.chunk.css"
+  },
+  {
+    "revision": "7e80a45ae228d315ad16",
+    "url": "/static/js/2.4a50aca8.chunk.js"
+  },
+  {
+    "revision": "5d1201625ec3a3eaf11d",
+    "url": "/static/js/main.6361034e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a14d0d685cc968a8d2fd926c64dcc895",
+    "url": "/static/media/BannerImage.a14d0d68.jpeg"
+  },
+  {
+    "revision": "623bc29ac0801bc714fc3de6f8695383",
+    "url": "/static/media/Cv.623bc29a.pdf"
+  },
+  {
+    "revision": "fc683e9b30ac7eb225f23b8cbea7bace",
+    "url": "/static/media/avatar.fc683e9b.jpg"
+  },
+  {
+    "revision": "7d117529e7bfe65a328ddd86d3ae61b0",
+    "url": "/static/media/background.7d117529.png"
+  },
+  {
+    "revision": "4ea472e6827323e859a3cb14273b5b7d",
+    "url": "/static/media/flag-us.4ea472e6.jpg"
+  },
+  {
+    "revision": "6774d436f1c040b10ed93b232978eeb9",
+    "url": "/static/media/icon-whatsapp.6774d436.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,13 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+  "/precache-manifest.c4c01c96398c83a4ae5fafac5529bf48.js"
 );
 
-importScripts('/precache-manifest.82a755c25b414cf6d52da6a585e163bc.js');
-
-self.addEventListener('message', event => {
+self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
@@ -33,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL('/index.html'),
-  {
-    blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
