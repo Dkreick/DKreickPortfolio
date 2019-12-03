6e1a2cccbaa5c1e246a8414226259339aaeccbab
DKreick <damian.kreick@gmail.com>
2019-12-03 15:05:24 -0300

    Connects to firebase database!

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import GameCard from './GameCard';
 import Grid from '@material-ui/core/Grid';
-import apps from './../../assets/data/apps.json';
 import Typography from '@material-ui/core/Typography';
 import ScrollAnimation from 'react-animate-on-scroll';
+import * as firebase from 'firebase';
 
 const styles = theme => ({
   container: {
@@ -25,51 +25,57 @@ const styles = theme => ({
   },
 });
 
-export function Portfolio(props) {
-  const { classes } = props;
 
-  return (
-    <div className={classes.container} id="portfolio">
-      <ScrollAnimation
-        animateIn="bounceInLeft"
-        animateOnce={true}
-        duration={1.5}
-      >
-        <Typography variant="h3" className={classes.title}>
-          Projects
+class Portfolio extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    let database = firebase.database().ref();
+    database.on('value', snap => {
+      console.log(snap.val())
+      this.setState({
+        data: snap.val()
+      })
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.container} id="portfolio">
+        <ScrollAnimation
+          animateIn="bounceInLeft"
+          animateOnce={true}
+          duration={1.5}
+        >
+          <Typography variant="h3" className={classes.title}>
+            Projects
         </Typography>
-      </ScrollAnimation>
-      <Grid container spacing={8}>
-        {apps.map((api, index) => {
-          return (
-            <Grid item xs={12} sm={3} key={index}>
-              <ScrollAnimation
-                animateIn="fadeIn"
-                duration={index + 1}
-                animateOnce={true}
-              >
-                <GameCard data={apps[index]} key={index} />
-              </ScrollAnimation>
-            </Grid>
-          );
-        })}
-      </Grid>
-      {/* <ScrollAnimation
-        animateIn="bounceInLeft"
-        animateOnce={true}
-        duration={1.5}
-      >
-        <Typography variant="h3" className={classes.title}>
-          Also I defend myself as an artist...
-        </Typography>
-      </ScrollAnimation>
-      <GridList className={classes.gridList} cols={2.5}>
-        {art.map((api, index) => {
-          return <ArtCard data={art[index]} key={index} />;
-        })}
-      </GridList> */}
-    </div>
-  );
+        </ScrollAnimation>
+        <Grid container spacing={8}>
+          {this.state.data.map((api, index) => {
+            return (
+              <Grid item xs={12} sm={3} key={index}>
+                <ScrollAnimation
+                  animateIn="fadeIn"
+                  duration={index + 1}
+                  animateOnce={true}
+                >
+                  <GameCard data={this.state.data[index]} key={index} />
+                </ScrollAnimation>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
+    );
+  }
 }
 
 Portfolio.propTypes = {
