9bc9a34195453fa8dfef9aee2bbd3731562341f4
DKreick <damian.kreick@gmail.com>
2020-04-22 16:16:34 -0300

    Updates

---
+++ precache-manifest.a40dd4f3f27d487e26a9df8b653cda1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e94cd208e72e6f3102133c94b222ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d8bba410b03342e9eb",
+    "url": "/static/css/main.ad7ada25.chunk.css"
+  },
+  {
+    "revision": "85a426c6b1b39a8c7a33",
+    "url": "/static/js/2.047b2409.chunk.js"
+  },
+  {
+    "revision": "c6d8bba410b03342e9eb",
+    "url": "/static/js/main.b81c12c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "623bc29ac0801bc714fc3de6f8695383",
+    "url": "/static/media/Cv.623bc29a.pdf"
+  },
+  {
+    "revision": "fc683e9b30ac7eb225f23b8cbea7bace",
+    "url": "/static/media/avatar.fc683e9b.jpg"
+  },
+  {
+    "revision": "6774d436f1c040b10ed93b232978eeb9",
+    "url": "/static/media/icon-whatsapp.6774d436.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c3f1afba715a76050fc95e8d4aeaf9f.js"
+  "/precache-manifest.a40dd4f3f27d487e26a9df8b653cda1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
